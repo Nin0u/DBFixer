a2db7956d6547422c41ba52b709ff696c3d46705
YE nicolas <nicolas.ye@etu.u-paris.fr>
2023-03-24 13:47:13 +0100

    Le programme prend maintenant un fichier ou rien

--- src/main/java/pied/Main.java
+++ src/main/java/pied/Main.java
@@ -1,11 +1,27 @@
 package pied;
 
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.File;
 import java.util.ArrayList;
 import contrainte.*;
 
 public class Main{
     public static void main(String[] args){
-        ArrayList<Contrainte> contraintes = Parser.parse(System.in);
+        InputStream is = System.in;
+        if (args.length > 1){
+            System.out.println("Nombre d'arguments invalides : accepte 0 ou 1 argument.");
+            return;
+        } else if (args.length == 1){
+            try {
+                is = new FileInputStream(new File(args[0]));
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found : " + args[0]);
+                return;
+            }
+        }
+        ArrayList<Contrainte> contraintes = Parser.parse(is);
 
         if (contraintes == null) System.out.println("contraintes null");
         else {
